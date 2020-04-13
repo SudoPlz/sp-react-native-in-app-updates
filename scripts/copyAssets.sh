44deee44d4d9fb554ff731697f88d0e92f16ad95
Ioannis Kokkinidis <sudoplz@gmail.com>
2020-04-13 14:14:07 +0300

    Added script to copy assets from main project to native-module

---
+++ scripts/copyAssets.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+INPUT_DIR=$1
+OUTPUT_DIR=$2
+
+cp -a $INPUT_DIR/android $OUTPUT_DIR
+cp -a $INPUT_DIR/ios $OUTPUT_DIR
+cp $INPUT_DIR/index.js $OUTPUT_DIR/index.js
+# cp -a $INPUT_DIR/lib $OUTPUT_DIR/lib
+cp $INPUT_DIR/sp-react-native-in-app-updates.podspec $OUTPUT_DIR/sp-react-native-in-app-updates.podspec
+cp $INPUT_DIR/README.md $OUTPUT_DIR/README.md
+
+
+echo "Copied android, ios, index.js, lib from $INPUT_DIR to $OUTPUT_DIR"
