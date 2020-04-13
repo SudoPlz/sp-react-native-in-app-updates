e203ec9a7808f22fc629ea58f94cecdde412e832
Ioannis Kokkinidis <sudoplz@gmail.com>
2020-04-13 16:56:10 +0300

    Implemented the Android side of the module

--- android/src/main/java/com/reactlibrary/SpInAppUpdatesModule.java
+++ android/src/main/java/com/reactlibrary/SpInAppUpdatesModule.java
@@ -1,24 +1,55 @@
 package com.reactlibrary;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.util.Log;
+
+import androidx.annotation.MainThread;
+
+import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.google.android.play.core.appupdate.AppUpdateInfo;
 import com.google.android.play.core.appupdate.AppUpdateManager;
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
+import com.google.android.play.core.install.InstallState;
+import com.google.android.play.core.install.InstallStateUpdatedListener;
 import com.google.android.play.core.install.model.AppUpdateType;
 import com.google.android.play.core.install.model.UpdateAvailability;
-import com.google.android.play.core.tasks.OnSuccessListener;
 import com.google.android.play.core.tasks.Task;
 
-public class SpInAppUpdatesModule extends ReactContextBaseJavaModule {
+import java.util.HashMap;
+import java.util.Map;
 
-    private final ReactApplicationContext reactContext;
+import static android.app.Activity.RESULT_OK;
+
+public class SpInAppUpdatesModule extends ReactContextBaseJavaModule implements InstallStateUpdatedListener, ActivityEventListener {
+
+    private AppUpdateManager appUpdateManager = null;
+
+    public static String UPDATE_COMPLETE = "update_complete";
+    public static String UPDATE_FAILED = "update_failed";
+
+    public static String IN_APP_UPDATE_RESULT_KEY = "in_app_update_result";
+    public static String IN_APP_UPDATE_STATE_KEY = "in_app_update_state";
+
+    public static int IN_APP_UPDATE_REQUEST_CODE = 34213956;
+
+    private boolean subscribedToUpdateStatuses = false;
+
 
     public SpInAppUpdatesModule(ReactApplicationContext reactContext) {
         super(reactContext);
-        this.reactContext = reactContext;
+        // Creates instance of the manager.
+        appUpdateManager = AppUpdateManagerFactory.create(this.getReactApplicationContext());
+        appUpdateManager.registerListener(this);
     }
 
     @Override
@@ -26,25 +57,128 @@ public class SpInAppUpdatesModule extends ReactContextBaseJavaModule {
         return "SpInAppUpdates";
     }
 
-    @ReactMethod
-    public void checkNeedsUpdate() {
-        //        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
-        // Creates instance of the manager.
-        AppUpdateManager appUpdateManager = AppUpdateManagerFactory.create(this.reactContext);
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
 
+        constants.put(IN_APP_UPDATE_RESULT_KEY, IN_APP_UPDATE_RESULT_KEY);
+        constants.put(IN_APP_UPDATE_STATE_KEY, IN_APP_UPDATE_STATE_KEY);
+
+        constants.put("UPDATE_AVAILABLE", UpdateAvailability.UPDATE_AVAILABLE);
+        constants.put("UPDATE_NOT_AVAILABLE", UpdateAvailability.UPDATE_NOT_AVAILABLE);
+        constants.put("UPDATE_UNKNOWN", UpdateAvailability.UNKNOWN);
+        constants.put("UPDATE_DEV_TRIGGERED", UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS);
+
+        constants.put("APP_UPDATE_IMMEDIATE", AppUpdateType.IMMEDIATE);
+        constants.put("APP_UPDATE_FLEXIBLE", AppUpdateType.FLEXIBLE);
+        return constants;
+    }
+
+    @ReactMethod
+    public void setStatusUpdateSubscription(Boolean active) {
+        subscribedToUpdateStatuses = active;
+    }
+
+    @ReactMethod
+    public void checkNeedsUpdate(Promise resolutionPromise) {
         // Returns an intent object that you use to check for an update.
         Task<AppUpdateInfo> appUpdateInfoTask = appUpdateManager.getAppUpdateInfo();
 
         // Checks that the platform will allow the specified type of update.
-        appUpdateInfoTask.addOnSuccessListener(new OnSuccessListener<AppUpdateInfo>() {
-            @Override
-            public void onSuccess(AppUpdateInfo appUpdateInfo) {
-                if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
-                        // For a flexible update, use AppUpdateType.FLEXIBLE
-                        && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
-                    // Request the update.
+        appUpdateInfoTask.addOnSuccessListener(appUpdateInfo -> {
+            WritableMap map = Arguments.createMap();
+            int availability = appUpdateInfo.updateAvailability();
+            map.putInt("updateAvailability", availability);
+            map.putBoolean("isImmediateUpdateAllowed", appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE));
+            map.putBoolean("isFlexibleUpdateAllowed", appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE));
+            Integer clientVersionStaleness = appUpdateInfo.clientVersionStalenessDays();
+            if (clientVersionStaleness != null) {
+                map.putInt("dayStaleness", clientVersionStaleness.intValue());
+            }
+
+            map.putInt("versionCode", appUpdateInfo.availableVersionCode());
+            String packageName = appUpdateInfo.packageName();
+            if (packageName != null) {
+                map.putString("packageName", packageName);
+            }
+            long totalBytes = appUpdateInfo.totalBytesToDownload();
+            if (totalBytes >= 0) {
+                map.putDouble("totalBytes", (double)totalBytes);
+            }
+            resolutionPromise.resolve(map);
+        });
+    }
+
+    @ReactMethod
+    public void startUpdate(int updateType, Promise resolutionPromise) {
+        // Returns an intent object that you use to check for an update.
+        Task<AppUpdateInfo> appUpdateInfoTask = appUpdateManager.getAppUpdateInfo();
+
+        // Checks that the platform will allow the specified type of update.
+        appUpdateInfoTask.addOnSuccessListener(appUpdateInfo -> {
+            if (appUpdateInfo.updateAvailability() != UpdateAvailability.UPDATE_AVAILABLE) {
+                resolutionPromise.reject("Error", "Update unavailable, check checkNeedsUpdate.updateAvailability first");
+            } else if (!appUpdateInfo.isUpdateTypeAllowed(updateType)) {
+                resolutionPromise.reject("Error", "Update type unavailable, check checkNeedsUpdate.isImmediateUpdateAllowed or checkNeedsUpdate.isFlexibleUpdateAllowed first.");
+            } else {
+                try {
+                    appUpdateManager.startUpdateFlowForResult(
+                        // Pass the intent that is returned by 'getAppUpdateInfo()'.
+                        appUpdateInfo,
+                        // Or 'AppUpdateType.FLEXIBLE' for flexible updates.
+                        AppUpdateType.IMMEDIATE,
+                        // The current activity making the update request.
+                        getCurrentActivity(),
+                        // Include a request code to later monitor this update request.
+                        IN_APP_UPDATE_REQUEST_CODE
+                    );
+                    resolutionPromise.resolve("Done");
+                } catch (IntentSender.SendIntentException e) {
+                    resolutionPromise.reject("SendIntentException","Error while starting the update flow: "+e.toString());
                 }
             }
         });
+    }
+
+
+    @Override
+    public void onStateUpdate(InstallState state) {
+        if (subscribedToUpdateStatuses) {
+            int curStatus = state.installStatus();
+            emitToJS(IN_APP_UPDATE_STATE_KEY, curStatus+"");
+        }
+    }
+
+    @Override
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+        if (requestCode != IN_APP_UPDATE_REQUEST_CODE) {
+            return;
+        }
+        emitToJS(IN_APP_UPDATE_RESULT_KEY, (resultCode == RESULT_OK ? UPDATE_COMPLETE : UPDATE_FAILED)+": result :"+resultCode);
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        // no-op
+    }
+
+
+    @MainThread
+    private boolean emitToJS(String key, String value) {
+        ReactContext reactContext = this.getReactApplicationContext();
+        if (reactContext == null || !reactContext.hasActiveCatalystInstance()) {
+            return false;
+        }
+
+        try {
+            reactContext.getJSModule(
+                DeviceEventManagerModule.RCTDeviceEventEmitter.class
+            ).emit(key, value);
+        } catch (Exception e) {
+            Log.wtf("InAppUpdates_EMITTER", "Error sending Event: sp_in_app_updates_" + key, e);
+            return false;
+        }
+
+        return true;
     }
 }
