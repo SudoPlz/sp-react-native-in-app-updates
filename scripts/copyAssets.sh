a70a918861915b03d76580fd635825b9c44855b0
Ioannis Kokkinidis <sudoplz@gmail.com>
2020-04-13 19:53:13 +0300

    Now copying the lib directory too

--- scripts/copyAssets.sh
+++ scripts/copyAssets.sh
@@ -6,7 +6,7 @@ OUTPUT_DIR=$2
 cp -a $INPUT_DIR/android $OUTPUT_DIR
 cp -a $INPUT_DIR/ios $OUTPUT_DIR
 cp $INPUT_DIR/index.js $OUTPUT_DIR/index.js
-# cp -a $INPUT_DIR/lib $OUTPUT_DIR/lib
+cp -a $INPUT_DIR/lib $OUTPUT_DIR
 cp $INPUT_DIR/sp-react-native-in-app-updates.podspec $OUTPUT_DIR/sp-react-native-in-app-updates.podspec
 cp $INPUT_DIR/README.md $OUTPUT_DIR/README.md
 
